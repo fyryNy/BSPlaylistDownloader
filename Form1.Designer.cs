a06981e52244f60fe174ff2df17c077ba634d767
fyryNy <fyryny@gmail.com>
2021-08-22 12:39:21 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace BSPlaylistDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 565);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(12, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Created by fyryNy";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(475, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Beat Saber location:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(12, 565);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "pre-alpha";
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(10, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(478, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = " ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 235);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(476, 250);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(288, 491);
+            this.progressBar1.Maximum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 20);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 25);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Select playlists";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(103, 491);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 20);
+            this.label5.TabIndex = 11;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(106, 517);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 20);
+            this.label6.TabIndex = 12;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Visible = false;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(288, 517);
+            this.progressBar2.Maximum = 1;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(200, 20);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 13;
+            this.progressBar2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::BSPlaylistDownloader.Properties.Resources.bspd_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 160);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(500, 600);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Beat Saber Playlist Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace BSPlaylistDownloader
+{
+    public partial class Form1 : Form
+    {
+        private string[] playlists;
+        private bool mouseDown;
+        private Point lastLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string location = ConfigurationManager.AppSettings.Get("BeatSaberLocation");
+            if (String.IsNullOrWhiteSpace(location) || !Directory.Exists(location))
+            {
+                textBox1.Text = "Double-click to select game location... (or paste it)";
+            }
+            else
+                textBox1.Text = location;
+
+            var appVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            label3.Text = String.Format("{0} (pre-alpha)", appVersion);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void textBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string location = textBox1.Text;
+            if (Directory.Exists(location) && File.Exists(location + "/Beat Saber.exe"))
+            {
+                label4.Text = "";
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                config.AppSettings.Settings.Remove("BeatSaberLocation");
+                config.AppSettings.Settings.Add("BeatSaberLocation", location);
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+
+                if (!Directory.Exists(location + "/Playlists"))
+                {
+                    richTextBox1.SelectionColor = Color.DarkRed;
+                    richTextBox1.AppendText("Directory \"Playlists\" does not exists.\n");
+                    Directory.CreateDirectory(location + "/Playlists");
+                    if (Directory.Exists(location + "/Playlists"))
+                    {
+                        richTextBox1.SelectionColor = Color.Green;
+                        richTextBox1.AppendText("Created \"Playlists\" directory.\n");
+                    }
+                    richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                }
+
+                if (!Directory.Exists(location + "/Beat Saber_Data/CustomLevels"))
+                {
+                    richTextBox1.SelectionColor = Color.DarkRed;
+                    richTextBox1.AppendText("Directory \"/Beat Saber_Data/CustomLevels\" does not exists.\n");
+                    Directory.CreateDirectory(location + "/Beat Saber_Data/CustomLevels");
+                    if (Directory.Exists(location + "/Beat Saber_Data/CustomLevels"))
+                    {
+                        richTextBox1.SelectionColor = Color.Green;
+                        richTextBox1.AppendText("Created \"/Beat Saber_Data/CustomLevels\" directory.\n");
+                    }
+                    richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                }
+
+                button2.Enabled = true;
+            }
+            else
+            {
+                label4.Text = "Please provide proper game path";
+                button2.Enabled = false;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                richTextBox1.SelectionColor = Color.Green;
+                richTextBox1.AppendText(String.Format("Loaded {0} playlists \n", openFileDialog1.FileNames.Length));
+                richTextBox1.SelectionColor = richTextBox1.ForeColor;
+                playlists = openFileDialog1.FileNames;
+
+                await RunParsePlaylistsAsync();
+
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                richTextBox1.AppendText(String.Format("\n\nTotal execution time: {0}ms \n", elapsedMs));
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private async Task RunParsePlaylistsAsync()
+        {
+            int playlistscount = playlists.Length;
+            progressBar1.Visible = true;
+            progressBar1.Maximum = playlistscount;
+            var progress1 = new Progress<int>(value =>
+            {
+                progressBar1.Value = value;
+            });
+            label5.Visible = true;
+            var label5text = new Progress<string>(value =>
+            {
+                label5.Text = value;
+            });
+
+            progressBar2.Visible = true;
+            var progress2 = new Progress<int>(value =>
+            {
+                progressBar2.Value = value;
+            });
+            label6.Visible = true;
+            var label6text = new Progress<string>(value =>
+            {
+                label6.Text = value;
+            });
+
+            var richbox1text = new Progress<string>(value =>
+            {
+                richTextBox1.AppendText(value);
+            });
+
+            await Task.Run(() => ParsePlaylists(progress1, label5text, progress2, label6text, richbox1text));
+
+            progressBar1.Value = progressBar1.Maximum;
+            label5.Text = String.Format("Completed");
+            label6.Visible = false;
+            progressBar2.Visible = false;
+        }
+
+        private void ParsePlaylists(IProgress<int> progress1, IProgress<string> label5text, IProgress<int> progress2, IProgress<string> label6text, IProgress<string> richbox1text)
+        {
+            int i = 0;
+            var regex = new Regex(@"\r\n?|\n|\t", RegexOptions.Compiled);
+            foreach (string file in playlists)
+            {
+                string jsonData = File.ReadAllText(file);
+                Playlist playlist = JsonConvert.DeserializeObject<Playlist>(jsonData);
+
+                string title = regex.Replace(playlist.playlistTitle, " - ");
+                string author = playlist.playlistAuthor;
+                int songcount = playlist.songs.Count;
+
+                progressBar2.Value = 0;
+                progressBar2.Maximum = songcount;
+                //label6.Text = String.Format("Downloading {0} songs", songcount);
+
+                //richTextBox1.AppendText(String.Format("Playlist \"{0}\" by {1} loaded, {2} songs. \n", title, author, songcount));
+                if (progress1 != null)
+                    progress1.Report(i++);
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+    public class Playlist
+    {
+        public string playlistTitle { get; set; }
+        public string playlistAuthor { get; set; }
+        public string playlistDescription { get; set; }
+        public string syncURL { get; set; }
+        public IList<Song> songs { get; set; }
+    }
+
+    public class Song
+    {
+        public string key { get; set; }
+        public string hash { get; set; }
+        public string songName { get; set; }
+        public string uploader { get; set; }
+    }
+}
